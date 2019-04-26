3c15f5ba879cf8373d89297021e5039a1bdf7be5
Daryl Newsholme <daryl.newsholme@linkgroup.co.uk>
2019-04-26 14:52:04 +0100

    added support for running as remote jobs and local job

--- functions/Publish-ListeningPort.ps1
+++ functions/Publish-ListeningPort.ps1
@@ -22,63 +22,89 @@ Daryl Newsholme
 #>
 
 function Publish-ListeningPort {
+    [cmdletbinding()]
     param(
 
         [Parameter(Mandatory = $true, Position = 0)][int][ValidateRange(0, 65535)]$port,
         [Parameter(Mandatory = $false, Position = 1)][switch]$exitonconnect,
         [Parameter(Mandatory = $false, Position = 2)][string]$RemoteDestination,
-        [Parameter(Mandatory = $false, Position = 3)][pscredential]$credential
+        [Parameter(Mandatory = $false, Position = 3)][pscredential]$credential,
+        [Parameter(Mandatory = $false, Position = 4)][switch]$asJob
+        
 
 
     )
-    if ($remotedestination){
-        Invoke-command -ComputerName $remotedestination -Credential $credential -ScriptBlock ${Function:Open-Port} -ArgumentList $port,$exitonconnect
+    if ($remotedestination) {
+        $parms = @{
+            Computername = $RemoteDestination
+            credential        = $credential
+        }
+        if ($asJob){
+            $parms += @{"AsJob"=$true}
+        }
+        Invoke-command  @parms -ScriptBlock ${Function:Open-Port} -ArgumentList $port, $exitonconnect
 
     }
     Else {
-        switch ($exitonconnect){
-            True{
-                Open-Port -port $port -exitonconnect
-
-            }
-            False {
-                Open-Port -port $port
-
-            }
+        if ($asJob){
+            Start-Job -ScriptBlock ${Function:Open-Port} -ArgumentList $port, $exitonconnect
         }
+        Else{
+            $parms = @{
+                port          = $port
+                exitonconnect = $exitonconnect
+            }
+            Open-Port @parms
+        }
+
+        
     }
 
 }
 
 Function Open-Port {
+    [cmdletbinding()]
     Param (
         [Parameter(Mandatory = $true, Position = 0)][int][ValidateRange(0, 65535)]$port,
         [Parameter(Mandatory = $false, Position = 1)][switch]$exitonconnect
     )
-    $endpoint = new-object System.Net.IPEndPoint ([system.net.ipaddress]::any, $port)
-    $listener = new-object System.Net.Sockets.TcpListener $endpoint
-    $listener.server.ReceiveTimeout = 3000
-    $listener.start()
-    try {
-        Write-Verbose "Listening on port $port, press CTRL+C to cancel"
-        While ($true) {
-            if (!$listener.Pending()) {
-                Start-Sleep -Seconds 1;
-                continue;
-            }
-            $client = $listener.AcceptTcpClient()
-            $client.client.RemoteEndPoint | Add-Member -NotePropertyName DateTime -NotePropertyValue (get-date) -PassThru
-            $client.close()
-            if ($exitonconnect) {
-                break
+    Begin {
+
+    }
+    Process {
+        # Open windows firewall
+        $rule = New-NetFirewallRule -DisplayName "[POWERSHELL PORT TEST] inbound TCP port $port" -Direction inbound -LocalPort $port -Protocol TCP -Action Allow
+        # Create endpoint and listen on port
+        $endpoint = new-object System.Net.IPEndPoint ([system.net.ipaddress]::any, $port)
+        $listener = new-object System.Net.Sockets.TcpListener $endpoint
+        $listener.server.ReceiveTimeout = 3000
+        $listener.start()
+        try {
+            Write-Verbose "$($env:ComputerName) on port $port, press CTRL+C to cancel" -Verbose
+            While ($true) {
+                if (!$listener.Pending()) {
+                    Start-Sleep -Seconds 1;
+                    continue;
+                }
+                $client = $listener.AcceptTcpClient()
+                $client.client.RemoteEndPoint | Add-Member -NotePropertyName DateTime -NotePropertyValue (get-date) -PassThru
+                $client.close()
+                if ($exitonconnect) {
+                    break
+                }
             }
         }
+        catch {
+            Write-Error $_
+        }
+        finally {
+            # Remove windows firewall exception
+            Remove-NetFirewallRule -Name $rule.Name
+            $listener.stop()
+
+        }
     }
-    catch {
-        Write-Error $_
-    }
-    finally {
-        $listener.stop()
+    End {
 
     }
 
